ace9498c54087d271a8e2216bdd9421022d45337
Bartek Fabiszewski <github@fabiszewski.net>
2017-04-13 08:58:40 +0200

    Add icon links to login page

--- auth.php
+++ auth.php
@@ -58,6 +58,14 @@ if (!$user->isValid && ($config::$require_authentication || defined('headless'))
         <title>' . $lang["title"] . '</title>
         <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
         <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+        <link rel="apple-touch-icon" sizes="180x180" href="icons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="icons/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="icons/favicon-16x16.png" sizes="16x16">
+        <link rel="manifest" href="manifest.json">
+        <link rel="mask-icon" href="icons/safari-pinned-tab.svg" color="#5bbad5">
+        <link rel="shortcut icon" href="icons/favicon.ico">
+        <meta name="msapplication-config" content="browserconfig.xml">
+        <meta name="theme-color" content="#ffffff">
         <link rel="stylesheet" type="text/css" href="css/main.css">
         <script type="text/javascript">
         function focus() {
