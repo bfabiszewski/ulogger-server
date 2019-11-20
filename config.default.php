2651ad38703b0ce56e7f99ce173879747f9733b1
René Wagner <apollo@rw-net.de>
2019-11-20 19:22:18 +0100

    set correct defaults

--- config.default.php
+++ config.default.php
@@ -57,9 +57,9 @@ $public_tracks = 0;
 
 // sort the track dropdown by name instead the default 'id' (which means time added)
 // (0 = no, 1 = yes)
-$tracks_sort_by_name = 1;
+$tracks_sort_by_name = 0;
 // revert sort direction for tracks to ascending, default is a descending order
-$tracks_sort_ascending = 1;
+$tracks_sort_ascending = 0;
 
 // admin user, who
 // - can add new users
