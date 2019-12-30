684a77f814ae218d527484d09135e38f64e3a99b
Bartek Fabiszewski <github@fabiszewski.net>
2019-12-30 14:01:56 +0100

    Remove console.log from production build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,24 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+        terserOptions: {
+          compress: {
+            pure_funcs: [ 'console.log' ]
+          }
+        }
+      })
+    ]
+  },
   module: {
     rules: [
       {
