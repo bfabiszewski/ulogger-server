ef5aaba9518cbe0ecf088aac87d138db3d88fe8f
Bartek Fabiszewski <github@fabiszewski.net>
2020-05-16 22:24:54 +0200

    Update node dependencies and dist bundle

--- webpack.common.js
+++ webpack.common.js
@@ -3,6 +3,16 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const csso = require('csso');
 const fs = require('fs');
+const cssDist = path.resolve(__dirname, 'css/dist');
+const cssTransform = (content, filename) => {
+  const basename = path.basename(filename);
+  const result = csso.minify(content.toString(), {
+    filename: basename,
+    sourceMap: true
+  });
+  fs.writeFile(`css/dist/${basename}.map`, result.map.toString(), (err) => { if (err) { throw err; }});
+  return `${result.css}\n/*# sourceMappingURL=${basename}.map */`;
+}
 
 module.exports = {
   entry: './js/src/ulogger.js',
@@ -15,22 +25,13 @@ module.exports = {
    new CleanWebpackPlugin({
       cleanOnceBeforeBuildPatterns: [ '**/*', '!.*' ]
     }),
-    new CopyWebpackPlugin([
-      {
-        from: '{css/src/*.css,node_modules/ol/ol.css,node_modules/chartist/dist/chartist.css}',
-        to: path.resolve(__dirname, 'css/dist'),
-        flatten: true,
-        transform: (content, filename) => {
-          const basename = path.basename(filename);
-          const result = csso.minify(content.toString(), {
-            filename: basename,
-            sourceMap: true
-          });
-          fs.writeFile(`css/dist/${basename}.map`, result.map.toString(), (err) => { if (err) { throw err; }});
-          return `${result.css}\n/*# sourceMappingURL=${basename}.map */`;
-        }
-      }
-    ])
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'css/src/*.css', to: cssDist, flatten: true, transform: cssTransform },
+        { from: 'node_modules/ol/ol.css', to: cssDist, flatten: true, transform: cssTransform },
+        { from: 'node_modules/chartist/dist/chartist.css', to: cssDist, flatten: true, transform: cssTransform }
+      ]
+    })
   ],
   optimization: {
     splitChunks: {
