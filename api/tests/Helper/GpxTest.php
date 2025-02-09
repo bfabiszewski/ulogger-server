10a88feb8c2dd5d0f20b42b740962097c209d31d
Bartek Fabiszewski <github@fabiszewski.net>
2025-02-09 18:24:34 +0100

    Add tests for gpx ulogger extensions

--- api/tests/Helper/GpxTest.php
+++ api/tests/Helper/GpxTest.php
@@ -12,6 +12,7 @@ namespace uLogger\Tests\Helper;
 use PHPUnit\Framework\MockObject\Exception;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use SimpleXMLElement;
 use uLogger\Component\Response;
 use uLogger\Entity\Config;
 use uLogger\Entity\File;
@@ -55,20 +56,38 @@ final class GpxTest extends TestCase {
    * @throws GpxParseException
    */
   public function testImportSuccess(): void {
-    // Create a temporary GPX file with valid content.
+
+    $trackName = 'Test track';
+    $trackComment = 'Test comment';
+    $trackId = 42;
+    $latitude = 10.15;
+    $longitude = -12.77;
+    $elevation = 58.3;
+    $time = '2025-01-01T10:56:33Z';
+    $description = '2025-01-01T10:56:33Z';
+
     $gpxContent = <<<XML
-<gpx>
-  <metadata>
-    <name>Test Metadata</name>
-  </metadata>
-  <trk>
-    <name>Test Track</name>
-    <trkseg>
-      <trkpt lat="12.34" lon="56.78">
-        <time>2023-01-01T12:00:00Z</time>
-      </trkpt>
-    </trkseg>
-  </trk>
+<?xml version="1.0" encoding="UTF-8"?>
+<gpx xmlns="http://www.topografix.com/GPX/1/1" 
+     xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd 
+     https://github.com/bfabiszewski/ulogger-android/1 https://raw.githubusercontent.com/bfabiszewski/ulogger-server/master/scripts/gpx_extensions1.xsd" 
+     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ulogger="https://github.com/bfabiszewski/ulogger-android/1" 
+     creator="μlogger-server 1.2" version="1.1">
+ <metadata>
+  <name>$trackComment</name>
+  <time>$time</time>
+ </metadata>
+ <trk>
+  <name>$trackName</name>
+  <trkseg>
+   <trkpt lat="$latitude" lon="$longitude">
+    <ele>$elevation</ele>
+    <time>$time</time>
+    <name>1</name>
+    <desc><![CDATA[$description]]></desc>
+   </trkpt>
+  </trkseg>
+ </trk>
 </gpx>
 XML;
     $tempFile = tempnam(sys_get_temp_dir(), 'gpx_');
@@ -78,16 +97,21 @@ XML;
     // Expect mapperTrack->create() to be called once.
     $this->trackMapperMock->expects($this->once())
       ->method('create')
-      ->with($this->callback(function ($track) {
-        // Simulate creation by setting an ID.
-        $track->id = 42;
+      ->with($this->callback(function ($track) use ($trackId) {
+        $track->id = $trackId;
         return true;
       }));
 
     // Expect mapperPosition->create() to be called once for the single track point.
     $this->positionMapperMock->expects($this->once())
       ->method('create')
-      ->with($this->isInstanceOf(Position::class));
+      ->with($this->callback(function(Position $position) use ($latitude, $longitude, $time, $elevation, $description) {
+        return $position->latitude === $latitude &&
+          $position->longitude === $longitude &&
+          $position->timestamp === strtotime($time) &&
+          $position->altitude === $elevation &&
+          $position->comment === $description;
+      }));
 
     $gpxImporter = new Gpx('Default Track Name', $this->configMock, $this->mapperFactoryMock);
     $tracks = $gpxImporter->import($userId, $tempFile);
@@ -98,9 +122,9 @@ XML;
     $this->assertIsArray($tracks);
     $this->assertCount(1, $tracks);
     $track = $tracks[0];
-    $this->assertEquals('Test Track', $track->name);
-    $this->assertEquals('Test Metadata', $track->comment);
-    $this->assertEquals(42, $track->id);
+    $this->assertEquals($trackName, $track->name);
+    $this->assertEquals($trackComment, $track->comment);
+    $this->assertEquals($trackId, $track->id);
   }
 
   /**
@@ -178,7 +202,12 @@ XML;
     $latitude = 12.34;
     $longitude = 56.78;
     $timestamp = 1;
+    $altitude = 12.34;
+    $comment = 'Test comment';
+
     $position = new Position($timestamp, 100, $trackId, $latitude, $longitude);
+    $position->altitude = $altitude;
+    $position->comment = $comment;
 
     $positions = [ $position ];
 
@@ -202,7 +231,147 @@ XML;
     $this->assertEquals((string) $latitude, (string) $xml->trk->trkseg->trkpt['lat']);
     $this->assertEquals((string) $longitude, (string) $xml->trk->trkseg->trkpt['lon']);
     $this->assertEquals(gmdate('Y-m-d\TH:i:s\Z', $timestamp), (string) $xml->trk->trkseg->trkpt->time);
+    $this->assertEquals((string) $altitude, (string) $xml->trk->trkseg->trkpt->ele);
+    $this->assertEquals($comment, (string) $xml->trk->trkseg->trkpt->desc);
     // name is point index
     $this->assertEquals('1', (string) $xml->trk->trkseg->trkpt->name);
   }
+
+  /**
+   * @throws DatabaseException
+   * @throws ServerException
+   * @throws GpxParseException
+   */
+  public function testImportWithUloggerExtensions(): void {
+
+    $trackName = 'Extended Track';
+    $trackComment = 'Test Metadata';
+    $trackId = 42;
+    $speed = 7.5;
+    $bearing = 45.0;
+    $accuracy = 10;
+    $provider = 'gps';
+    $comment = 'Test description';
+
+    $gpxContent = <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<gpx xmlns="http://www.topografix.com/GPX/1/1" 
+     xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd 
+     https://github.com/bfabiszewski/ulogger-android/1 https://raw.githubusercontent.com/bfabiszewski/ulogger-server/master/scripts/gpx_extensions1.xsd" 
+     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ulogger="https://github.com/bfabiszewski/ulogger-android/1" 
+     creator="μlogger-server 1.2" version="1.1">
+  <metadata>
+    <name>$trackComment</name>
+  </metadata>
+  <trk>
+    <name>$trackName</name>
+    <trkseg>
+      <trkpt lat="12.34" lon="56.78">
+        <time>2023-01-01T12:00:00Z</time>
+        <desc>$comment</desc>
+        <extensions>
+          <ulogger:speed xmlns:ulogger="https://github.com/bfabiszewski/ulogger-android/1">$speed</ulogger:speed>
+          <ulogger:bearing xmlns:ulogger="https://github.com/bfabiszewski/ulogger-android/1">$bearing</ulogger:bearing>
+          <ulogger:accuracy xmlns:ulogger="https://github.com/bfabiszewski/ulogger-android/1">$accuracy</ulogger:accuracy>
+          <ulogger:provider xmlns:ulogger="https://github.com/bfabiszewski/ulogger-android/1">$provider</ulogger:provider>
+        </extensions>
+      </trkpt>
+    </trkseg>
+  </trk>
+</gpx>
+XML;
+    // Write GPX content to a temporary file.
+    $tempFile = tempnam(sys_get_temp_dir(), 'gpx_');
+    file_put_contents($tempFile, $gpxContent);
+    $userId = 100;
+
+    $this->trackMapperMock->expects($this->once())
+      ->method('create')
+      ->willReturnCallback(function($track) use ($trackId) {
+        $track->id = $trackId;
+      });
+
+    $this->positionMapperMock->expects($this->once())
+      ->method('create')
+      ->with($this->callback(function(Position $position) use ($speed, $bearing, $accuracy, $provider, $comment) {
+        return abs($position->speed - $speed) < 0.0001 &&
+          abs($position->bearing - $bearing) < 0.0001 &&
+          $position->accuracy === $accuracy &&
+          $position->provider === $provider &&
+          $position->comment === $comment;
+      }));
+
+    $gpxImporter = new Gpx('Default Track Name', $this->configMock, $this->mapperFactoryMock);
+    $tracks = $gpxImporter->import($userId, $tempFile);
+
+    // Cleanup temporary file.
+    unlink($tempFile);
+
+    // Assert that a track was imported.
+    $this->assertIsArray($tracks);
+    $this->assertCount(1, $tracks);
+    $track = $tracks[0];
+    $this->assertEquals($trackName, $track->name);
+    $this->assertEquals($trackComment, $track->comment);
+    $this->assertEquals($trackId, $track->id);
+  }
+
+  /**
+   * @throws ServerException
+   */
+  public function testExportIncludesUloggerExtensions(): void {
+
+    $speed = 7.5;
+    $bearing = 45.0;
+    $accuracy = 10;
+    $provider = 'gps';
+
+    $position = new Position(strtotime('2023-01-01T12:00:00Z'), 1, 1, 12.34, 56.78);
+    $position->speed = $speed;
+    $position->bearing = $bearing;
+    $position->accuracy = $accuracy;
+    $position->provider = $provider;
+    $positions = [ $position ];
+
+    $gpx = new Gpx('Test GPX', $this->configMock, $this->mapperFactoryMock);
+    $file = $gpx->export($positions);
+
+    $this->assertInstanceOf(File::class, $file);
+    $this->assertSame('track1.gpx', $file->getFileName());
+    $this->assertSame(Response::TYPE_GPX, $file->getMimeType());
+
+    $xml = simplexml_load_string($file->getContent());
+    $this->assertInstanceOf(SimpleXMLElement::class, $xml);
+
+    $xml->registerXPathNamespace('gpx', 'http://www.topografix.com/GPX/1/1');
+    $xml->registerXPathNamespace('ulogger', 'https://github.com/bfabiszewski/ulogger-android/1');
+
+    // Locate the track point (<trkpt>) element.
+    $trkpts = $xml->xpath('//gpx:trkpt');
+    $this->assertNotEmpty($trkpts, 'Expected at least one <trkpt> element in the exported GPX.');
+    $trkpt = $trkpts[0];
+
+    $trkpt->registerXPathNamespace('gpx', 'http://www.topografix.com/GPX/1/1');
+    $trkpt->registerXPathNamespace('ulogger', 'https://github.com/bfabiszewski/ulogger-android/1');
+
+    $extensions = $trkpt->xpath('gpx:extensions');
+    $this->assertNotEmpty($extensions, 'Expected a <extensions> element in the <trkpt>.');
+
+    $speedNodes = $trkpt->xpath('gpx:extensions/ulogger:speed');
+    $this->assertNotEmpty($speedNodes, 'Expected <ulogger:speed> element.');
+    $this->assertSame($speed, (float) $speedNodes[0]);
+
+    $bearingNodes = $trkpt->xpath('gpx:extensions/ulogger:bearing');
+    $this->assertNotEmpty($bearingNodes, 'Expected <ulogger:bearing> element.');
+    $this->assertEquals($bearing, (float) $bearingNodes[0]);
+
+    $accuracyNodes = $trkpt->xpath('gpx:extensions/ulogger:accuracy');
+    $this->assertNotEmpty($accuracyNodes, 'Expected <ulogger:accuracy> element.');
+    $this->assertSame($accuracy, (int) $accuracyNodes[0]);
+
+    $providerNodes = $trkpt->xpath('gpx:extensions/ulogger:provider');
+    $this->assertNotEmpty($providerNodes, 'Expected <ulogger:provider> element.');
+    $this->assertSame($provider, (string) $providerNodes[0]);
+  }
+
 }
